e37e79f3022e72984da5b78d1965b199b75b554a
Neto Andrey <diomand761988@gmail.com>
2026-04-17 00:32:22 +0300

    Add AppOrderPositiveTest

--- src/test/java/ru/netology/selenium/AppOrderPositiveTest.java
+++ src/test/java/ru/netology/selenium/AppOrderPositiveTest.java
@@ -5,32 +5,53 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-class AppOrderPositiveTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppOrderPositiveTest {
     private WebDriver driver;
 
     @BeforeAll
     public static void setupAll() {
         WebDriverManager.chromedriver().setup();
     }
+
     @BeforeEach
-    void setUp() {
+    public void beforeEach() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
+
     @AfterEach
-    public void tearDown() {
+    public void afterEach() {
         driver.quit();
         driver = null;
     }
+
     @Test
-    public void shouldTestSomething() {
-//TODO реализовать логику автотеста
+    public void successfulForm() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+
+        form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Петров Иван");
+        form.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78001111111");
+        form.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        form.findElement(By.cssSelector("button")).click();
+
+        WebElement actualElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+
+        String actualText = actualElement.getText().trim();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        assertTrue(actualElement.isDisplayed());
     }
 }
